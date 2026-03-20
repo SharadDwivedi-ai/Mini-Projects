050bafe2b5dc767253b9a8f6fb805ada814f7219
Sharad Dwivedi <srd28nov21@gmail.com>
2026-03-21 01:14:06 +0530

    Body background change with setTimeout event

---
+++ 05_Unlimited Colors/script.js
@@ -0,0 +1,26 @@
+//  Generate the random color 
+const randomColor = function () {
+    let hex = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += hex[Math.floor(Math.random() * 16)]
+    }
+    return color;
+}
+
+let intervalId;
+const startColorChange = function () {
+    if (!intervalId) {
+        intervalId = setInterval(changeBgColor, 1000)
+    }
+    function changeBgColor() {
+        document.body.style.backgroundColor = randomColor()
+    }
+}
+const stopColorChange = function () {
+    clearInterval(intervalId);
+    intervalId = null;
+}
+document.querySelector('#start').addEventListener('click', startColorChange)
+document.querySelector('#stop').addEventListener('click', stopColorChange)
+
